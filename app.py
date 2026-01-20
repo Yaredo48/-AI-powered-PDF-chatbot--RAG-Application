35d3aa2b2b5a21c096173e91cd823a7056362ddc
yared <yaredberhanu246@gmail.com>
2026-01-20 12:47:56 -0500

    Enhance user interface for PDF chatbot with styled chat messages and conversation history download feature

--- app.py
+++ app.py
@@ -85,4 +85,169 @@ def user_input(user_question, model_name, api_key, pdf_docs, conversation_histor
          pdf_names = [pdf.name for pdf in pdf_docs] if pdf_docs else []
          conversation_history.append((user_question_output, response_output, model_name, datetime.now().strftime('%Y-%m-%d %H:%M:%S'), ", ".join(pdf_names)))
 
+
+    st.markdown(
+        f"""
+        <style>
+            .chat-message {{
+                padding: 1.5rem;
+                border-radius: 0.5rem;
+                margin-bottom: 1rem;
+                display: flex;
+            }}
+            .chat-message.user {{
+                background-color: #2b313e;
+            }}
+            .chat-message.bot {{
+                background-color: #475063;
+            }}
+            .chat-message .avatar {{
+                width: 20%;
+            }}
+            .chat-message .avatar img {{
+                max-width: 78px;
+                max-height: 78px;
+                border-radius: 50%;
+                object-fit: cover;
+            }}
+            .chat-message .message {{
+                width: 80%;
+                padding: 0 1.5rem;
+                color: #fff;
+            }}
+            .chat-message .info {{
+                font-size: 0.8rem;
+                margin-top: 0.5rem;
+                color: #ccc;
+            }}
+        </style>
+        <div class="chat-message user">
+            <div class="avatar">
+                <img src="https://i.ibb.co/CKpTnWr/user-icon-2048x2048-ihoxz4vq.png">
+            </div>    
+            <div class="message">{user_question_output}</div>
+        </div>
+        <div class="chat-message bot">
+            <div class="avatar">
+                <img src="https://i.ibb.co/wNmYHsx/langchain-logo.webp" >
+            </div>
+            <div class="message">{response_output}</div>
+            </div>
+            
+        """,
+        unsafe_allow_html=True
+    )
+            # <div class="info" style="margin-left: 20px;">Timestamp: {datetime.now()}</div>
+            # <div class="info" style="margin-left: 20px;">PDF Name: {", ".join(pdf_names)}</div>
+    if len(conversation_history) == 1:
+        conversation_history = []
+    elif len(conversation_history) > 1 :
+        last_item = conversation_history[-1]  # Son öğeyi al
+        conversation_history.remove(last_item) 
+    for question, answer, model_name, timestamp, pdf_name in reversed(conversation_history):
+        st.markdown(
+            f"""
+            <div class="chat-message user">
+                <div class="avatar">
+                    <img src="https://i.ibb.co/CKpTnWr/user-icon-2048x2048-ihoxz4vq.png">
+                </div>    
+                <div class="message">{question}</div>
+            </div>
+            <div class="chat-message bot">
+                <div class="avatar">
+                    <img src="https://i.ibb.co/wNmYHsx/langchain-logo.webp" >
+                </div>
+                <div class="message">{answer}</div>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+                # <div class="info" style="margin-left: 20px;">Timestamp: {timestamp}</div>
+                # <div class="info" style="margin-left: 20px;">PDF Name: {pdf_name}</div>
+
+    if len(st.session_state.conversation_history) > 0:
+        df = pd.DataFrame(st.session_state.conversation_history, columns=["Question", "Answer", "Model", "Timestamp", "PDF Name"])
+
+        # df = pd.DataFrame(st.session_state.conversation_history, columns=["Question", "Answer", "Timestamp", "PDF Name"])
+        csv = df.to_csv(index=False)
+        b64 = base64.b64encode(csv.encode()).decode()  # Convert to base64
+        href = f'<a href="data:file/csv;base64,{b64}" download="conversation_history.csv"><button>Download conversation history as CSV file</button></a>'
+        st.sidebar.markdown(href, unsafe_allow_html=True)
+        st.markdown("To download the conversation, click the Download button on the left side at the bottom of the conversation.")
+    st.snow()
+def main():
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+    st.header("Chat with multiple PDFs (v1) :books:")
+
+    if 'conversation_history' not in st.session_state:
+        st.session_state.conversation_history = []
+    linkedin_profile_link = "https://www.linkedin.com/in/snsupratim/"
+    kaggle_profile_link = "https://www.kaggle.com/snsupratim/"
+    github_profile_link = "https://github.com/snsupratim/"
+
+    st.sidebar.markdown(
+        f"[![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)]({linkedin_profile_link}) "
+        f"[![Kaggle](https://img.shields.io/badge/Kaggle-20BEFF?style=for-the-badge&logo=kaggle&logoColor=white)]({kaggle_profile_link}) "
+        f"[![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)]({github_profile_link})"
+    )
+
+
+
+    model_name = st.sidebar.radio("Select the Model:", ( "Google AI"))
+
+    api_key = None
+
+    if model_name == "Google AI":
+        api_key = st.sidebar.text_input("Enter your Google API Key:")
+        st.sidebar.markdown("Click [here](https://ai.google.dev/) to get an API key.")
         
+        if not api_key:
+            st.sidebar.warning("Please enter your Google API Key to proceed.")
+            return
+
+   
+    with st.sidebar:
+        st.title("Menu:")
+        
+        col1, col2 = st.columns(2)
+        
+        reset_button = col2.button("Reset")
+        clear_button = col1.button("Rerun")
+
+        if reset_button:
+            st.session_state.conversation_history = []  # Clear conversation history
+            st.session_state.user_question = None  # Clear user question input 
+            
+            
+            api_key = None  # Reset Google API key
+            pdf_docs = None  # Reset PDF document
+            
+        else:
+            if clear_button:
+                if 'user_question' in st.session_state:
+                    st.warning("The previous query will be discarded.")
+                    st.session_state.user_question = ""  # Temizle
+                    if len(st.session_state.conversation_history) > 0:
+                        st.session_state.conversation_history.pop()  # Son sorguyu kaldır
+                else:
+                    st.warning("The question in the input will be queried again.")
+
+
+
+
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            if pdf_docs:
+                with st.spinner("Processing..."):
+                    st.success("Done")
+            else:
+                st.warning("Please upload PDF files before processing.")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question, model_name, api_key, pdf_docs, st.session_state.conversation_history)
+        st.session_state.user_question = ""  # Clear user question input 
+
+if __name__ == "__main__":
+    main()
